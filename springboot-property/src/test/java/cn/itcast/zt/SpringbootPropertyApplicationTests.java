58938b4a47ddf5dc756f9a957836617b28c8cae9
zt971064178 <zhang.tian20080808@163.com>
2017-04-08 17:41:16 +0800

    修改

--- springboot-property/src/test/java/cn/itcast/zt/SpringbootPropertyApplicationTests.java
+++ springboot-property/src/test/java/cn/itcast/zt/SpringbootPropertyApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(classes = SpringbootPropertyApplication.class)
 public class SpringbootPropertyApplicationTests {
 	private static final Log log = LogFactory.getLog(SpringbootPropertyApplicationTests.class);
 
