287187bd6ea193d4d607d5e8cf0a9532e3069c11
zhangtian <zhang.tian20080808>
2017-04-12 10:03:36 +0800

    springboot集成mybatis操作数据库详解

---
+++ springboot-mybatis-complex/src/main/java/cn/itcast/zt/domain/User.java
@@ -0,0 +1,43 @@
+package cn.itcast.zt.domain;
+
+/**
+ * Created by zhangtian on 2017/4/12.
+ */
+public class User {
+   private Long id ;
+   private String name ;
+   private Integer age ;
+
+   public User(){
+
+   }
+
+   public User(String name, Integer age) {
+       this.name = name ;
+       this.age = age ;
+   }
+
+   public Long getId() {
+        return id;
+    }
+
+   public void setId(Long id) {
+        this.id = id;
+    }
+
+   public String getName() {
+        return name;
+    }
+
+   public void setName(String name) {
+        this.name = name;
+    }
+
+   public Integer getAge() {
+        return age;
+    }
+
+   public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ springboot-mybatis-complex/src/test/java/cn/itcast/zt/SpringbootMybatisComplexApplicationTests.java
@@ -0,0 +1,60 @@
+package cn.itcast.zt;
+
+import cn.itcast.zt.domain.User;
+import cn.itcast.zt.domain.UserMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringbootMybatisComplexApplication.class)
+@Transactional
+public class SpringbootMybatisComplexApplicationTests {
+
+	@Autowired
+	private UserMapper userMapper ;
+
+	@Test
+	@Rollback
+	public void testUserMapper() throws Exception {
+		// insert一条数据，并select出来验证
+		userMapper.insert("MMM", 20);
+		User u = userMapper.findByName("MMM");
+
+		// update一条数据，并select出来验证
+		u.setAge(30);
+		userMapper.update(u);
+		u = userMapper.findByName("MMM");
+		Assert.assertEquals(30, u.getAge().intValue());
+
+		// 删除这条数据，并select验证
+		userMapper.delete(u.getId());
+		u = userMapper.findByName("MMM");
+		Assert.assertEquals(null, u);
+
+		u = new User("BBB", 30);
+		userMapper.insertByUser(u);
+		Assert.assertEquals(30, userMapper.findByName("BBB").getAge().intValue());
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "CCC");
+		map.put("age", 40);
+		userMapper.insertByMap(map);
+		Assert.assertEquals(40, userMapper.findByName("CCC").getAge().intValue());
+
+		List<User> userList = userMapper.findAll();
+		for(User user : userList) {
+			Assert.assertEquals(null, user.getId());
+			Assert.assertNotEquals(null, user.getName());
+		}
+	}
+}
