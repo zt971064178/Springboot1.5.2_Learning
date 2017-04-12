23930008fecf12db003a0c0f5698b88fa0782e3d
zhangtian <zhang.tian20080808>
2017-04-12 20:48:56 +0800

    springboot替换log4j进行日志控制，并且将日志写入到mongoDB

---
+++ springboot-log4j-mongodb/src/main/java/cn/itcast/zt/SpringbootLog4jMongodbApplication.java
@@ -0,0 +1,12 @@
+package cn.itcast.zt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLog4jMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLog4jMongodbApplication.class, args);
+	}
+}
