c8c80f26e14d26dae3c0e65b5da8f4afe472819d
zhangtian <zhang.tian20080808>
2017-04-13 13:40:17 +0800

    springboot安全控制security

---
+++ springboot-security/src/main/java/cn/itcast/zt/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package cn.itcast.zt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by zhangtian on 2017/4/13.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll() ;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER") ;
+    }
+}
---
+++ springboot-security/src/main/java/cn/itcast/zt/web/HelloController.java
@@ -0,0 +1,26 @@
+package cn.itcast.zt.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by zhangtian on 2017/4/13.
+ */
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+}
