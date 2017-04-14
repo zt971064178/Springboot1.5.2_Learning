87232c2893398c7db21fdd2789ce4dc3f228c0d2
zhangtian <zhang.tian20080808>
2017-04-14 17:31:37 +0800

    springboot状态机管理

---
+++ springoot-statemachine/src/main/java/cn/itcast/zt/Events.java
@@ -0,0 +1,9 @@
+package cn.itcast.zt;
+
+/**
+ * Created by zhangtian on 2017/4/14.
+ */
+public enum Events {
+    PAY,        // 支付
+    RECEIVE    // 收货
+}
---
+++ springoot-statemachine/src/main/java/cn/itcast/zt/SpringootStatemachineApplication.java
@@ -0,0 +1,25 @@
+package cn.itcast.zt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+
+@SpringBootApplication
+public class SpringootStatemachineApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringootStatemachineApplication.class, args);
+	}
+
+	@Autowired
+	private StateMachine<States, Events> stateMachine ;
+
+	@Override
+	public void run(String... args) throws Exception {
+		stateMachine.start();
+		stateMachine.sendEvent(Events.PAY);
+		stateMachine.sendEvent(Events.RECEIVE);
+	}
+}
