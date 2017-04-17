670fd66cd5e762001a152614ac146dad13386149
zhangtian <zhang.tian20080808>
2017-04-17 17:15:12 +0800

    数据驱动更新

--- springboot-javamail/src/main/java/cn/itcast/zt/SpringbootJavamailApplication.java
+++ springboot-javamail/src/main/java/cn/itcast/zt/SpringbootJavamailApplication.java
@@ -1,11 +1,9 @@
 package cn.itcast.zt;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class SpringbootJavamailApplication {
 
 	public static void main(String[] args) {
