95279f2e966fcbbded3d10425613a79db6f09810
zhangtian <zhang.tian20080808>
2017-05-05 14:35:54 +0800

    docker安装redis集群

--- springboot-redis/src/main/java/cn/itcast/zt/SpringbootRedisApplication.java
+++ springboot-redis/src/main/java/cn/itcast/zt/SpringbootRedisApplication.java
@@ -3,6 +3,10 @@ package cn.itcast.zt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * docker搭建redis集群
+ * https://segmentfault.com/a/1190000004353368
+ */
 @SpringBootApplication
 public class SpringbootRedisApplication {
 
